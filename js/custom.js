1a8c80639b2c975f2a741a7f7713ae5962ad9add
nayonour294 <101937739+nayonour294@users.noreply.github.com>
2023-08-04 05:37:33 +0600

    change
    change codle

--- js/custom.js
+++ js/custom.js
@@ -17,6 +17,44 @@ setInterval(() => {
 }, 2000);
 
 
+const backgroundChange = document.querySelectorAll(".background-change");
+const sliderImages = document.querySelector(".slider-images");
+
+let sliderCount = 1;
+
+setInterval(() => {
+    sliderCount++;
+    const currentImage = document.querySelector(".current-img");
+    currentImage.classList.remove("current-img");
+    if (sliderCount > backgroundChange.length) {
+        backgroundChange[0].classList.add("current-img");
+        sliderCount = 1;
+    } else {
+        currentImage.nextElementSibling.classList.add("current-img");
+    }
+    
+}, 2000);
+
+
+const contentChange = document.querySelectorAll(".content-change");
+const sliderContents = document.querySelector(".slider-contents");
+
+let contentSliderCount = 1;
+
+setInterval(() => {
+    contentSliderCount++;
+    const currentContent = document.querySelector(".current-content");
+    currentContent.classList.remove("current-content");
+    if (contentSliderCount > contentChange.length) {
+        contentChange[0].classList.add("current-content");
+        contentSliderCount = 1;
+    } else {
+        currentContent.nextElementSibling.classList.add("current-content");
+    }
+    
+}, 2000);
+
+
 
 // const stopSlide = () => {
 //     count++
